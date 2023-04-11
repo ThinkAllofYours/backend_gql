b826349662d97ca87c05ec64169d67efce6f73fa
DuhwanJang <lyder86@puroom.net>
2023-04-11 14:20:01 +0900

    init

---
+++ backend_gql/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from backend_gql.users.models import User
+from backend_gql.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
